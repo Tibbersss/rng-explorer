14540db4412d71d036ade696dc54d5c2905e4349
tibbers <tibbers@vip.qq.com>
2019-04-09 14:11:06 +0800

    add socket event UPDATE_LATEST_UNITS

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,17 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+
 Vue.use(Vuex)
 const store = new Vuex.Store({
   state: {
+    units: []
+  },
+  mutations: {
   },
   actions: {
+    'SOCKET_UPDATE_LATEST_UNITS'({state}, data) {
+      state.units = data
+    }
   }
 })
 
