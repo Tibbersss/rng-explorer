092bbf7cce4a8c1fc5e4f62f5eb2d1e05de4dda0
Tibbers <tibbers@vip.qq.com>
2019-04-10 01:11:22 +0800

    set browser language to i18n

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -2,11 +2,16 @@ import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 
 Vue.use(VueI18n)
-
+let locale = navigator.language.indexOf('zh') > -1 ? 'zh' : 'en'
 const i18n = new VueI18n({
-  locale: 'en',
+  locale: locale,
   messages: {
-    en: {},
+    zh: {
+      index: '首页'
+    },
+    en: {
+      index: 'index'
+    },
   },
 })
 
